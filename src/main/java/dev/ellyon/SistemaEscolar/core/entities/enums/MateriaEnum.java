51c4aeadb00a616c5ae9891d8302c748ee085860
gabeFrancisco <gabegit2@gmail.com>
2025-10-14 21:46:45 -0300

    criado RoleEnum e MateriaEnum

---
+++ src/main/java/dev/ellyon/SistemaEscolar/core/entities/enums/MateriaEnum.java
@@ -0,0 +1,15 @@
+package dev.ellyon.SistemaEscolar.core.entities.enums;
+
+public enum MateriaEnum {
+  PORTUGUES,
+  ESPANHOL,
+  MATEMATICA,
+  FISICA,
+  QUIMICA,
+  HISTORIA,
+  LITERATURA,
+  ED_FISICA,
+  INGLES,
+  GEOGRAFIA,
+  ARTES
+}
---
+++ src/main/java/dev/ellyon/SistemaEscolar/core/entities/enums/RoleEnum.java
@@ -0,0 +1,6 @@
+package dev.ellyon.SistemaEscolar.core.entities.enums;
+
+public enum RoleEnum {
+  PROFESSOR,
+  COORDEMADOR
+}
